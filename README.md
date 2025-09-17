db0ffac21e445806ea78cf410274d7c01d48f547
Andej Horny <horny.andrej@dpb.sk>
2025-09-17 16:59:40 +0200

    fixed namespaces

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TO DO 
 
-vykonana praca na niecom
+user morph to treba nejako osetrit cez config napr
--- src/Listeners/LogStateChangeListener.php
+++ src/Listeners/LogStateChangeListener.php
@@ -3,7 +3,8 @@
 namespace Dpb\Extension\ModelState\Listeners;
 
 use Spatie\ModelStates\Events\StateChanged;
-use Dpb\Extension\ModelState\StateChange;
+use Dpb\Extension\ModelState\Models\StateChange;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Guard;
 
 class LogStateChangeListener
@@ -14,13 +15,14 @@ class LogStateChangeListener
     {
         // Resolve the actor
         $userId = $this->guard->id() ?? null; // fallback if queued job stored actor
+        $userType = $this->guard?->user->getMorphClass() ?? config('ext-spatie-model-states.user_morph_uri', Authenticatable::class);
 
         StateChange::create([
             'model_type' => $event->model->getMorphClass(),
             'model_id'   => $event->model->id,
             'from_state' => $event->initialState,
             'to_state'   => $event->finalState,
-            'user_type'    => $userId,
+            'user_type'    => $userType, //config('ext-spatie-model-states.user_morph_uri'),
             'user_id'    => $userId,
             'changed_at' => now(),
             'source'     => app()->runningInConsole() ? 'system' : 'http',
